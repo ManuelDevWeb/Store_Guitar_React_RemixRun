e9765e5583c0686b53c3e467791df286d7795f8c
ManuelDev <manueldeveloper17@gmail.com>
2022-10-19 00:00:36 -0500

    Styles to guitarras

--- app/routes/tienda.jsx
+++ app/routes/tienda.jsx
@@ -4,12 +4,37 @@ import { useLoaderData } from "@remix-run/react";
 import { getGuitarras } from "../models/guitarras.server";
 
 // Componentes
-import Guitarra from "~/components/guitarra";
+import Guitarra from "../components/guitarra";
+
+// Hoja de estilos
+import styles from "../styles/guitarras.css";
+
+// Funcion para agregar informacion meta al componente Meta que nos provee remix run
+// Ya no toca llamar el componente Meta, puesto se agrego en el archivo root.jsx
+export function meta() {
+  return {
+    title: "GuitarLA - Tienda",
+    description: "Nuestra colección de guitarras",
+  };
+}
+
+// Funcion para agregar hojas de estilo y link al componente Link que nos provee remix run
+// Ya no toca llamar el componente Link, puerto se agrego en el archivo root.jsx
+export function links() {
+  // Arreglo con cada uno de los objetos con la hoja de estilos a agregar
+  return [
+    // Hoja de estilos local
+    {
+      rel: "stylesheet",
+      href: styles,
+    },
+  ];
+}
 
 // Funcion loader (Loader es lo que se usa cuando el componente va a cargar datos)
 export async function loader() {
   const guitarras = await getGuitarras();
-  return guitarras;
+  return guitarras.data;
 }
 
 const Tienda = () => {
